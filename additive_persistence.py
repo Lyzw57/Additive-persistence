5d5d1ecfb16d1cc5a25e44b10832fa79b1f39a90
Lyzw57 <askuczynski57@gmail.com>
2019-02-24 12:08:04 +0100

    created add_persistence function and solve the problem using strings

--- additive_persistence.py
+++ additive_persistence.py
@@ -2,4 +2,14 @@ def add_digits(number):
     digits = [int(digit) for digit in str(number)]
     return sum(digits)
 
+def add_persistence(number):
+    persistence = 0
+    while number > 9:
+        number = add_digits(number)
+        persistence += 1
+   
+    return persistence
 
+if __name__ == '__main__':
+    for example in (13, 1234, 9876, 199):
+        print(example, add_persistence(example))
